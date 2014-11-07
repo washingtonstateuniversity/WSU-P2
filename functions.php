617b02375b6e3bc6e75304ff6ad792a7fa85c3db
jeremyfelt <jeremy.felt@gmail.com>
2014-11-06 17:03:46 -0800

    Introduce P2 Resolved Posts 0.3.3

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@
 // Include the Mention Me Widget from Thorston Ott
 require_once( get_template_directory() . '/plugins/mention-me/mention-me.php' );
 
+// Include the P2 Resolved Posts plugin from Automattic (via Daniel Bachhuber, via Andrew Nacing)
+require_once( get_template_directory() . '/plugins/p2-resolved-posts/p2-resolved-posts.php' );
+
 require_once( get_template_directory() . '/inc/utils.php' );
 
 p2_maybe_define( 'P2_INC_PATH', get_template_directory()     . '/inc' );
